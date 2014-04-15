d5dd3c709401bf45b15e5512464d2355286ce5b5
fideloper <fideloper@gmail.com>
2014-04-14 20:42:16 -0400

    a shell of an entitymapper. need adjustments before proceeding

--- src/EntityMapper/EntityMapper.php
+++ src/EntityMapper/EntityMapper.php
@@ -1,20 +1,61 @@
-<?php  namespace EntityMapper; 
+<?php  namespace EntityMapper;
 
+use ReflectionClass;
+use EntityMapper\Cache\EntityCache;
+
+/**
+ * Map/Hydrate an Entity
+ * Class EntityMapper
+ * @package EntityMapper
+ */
 class EntityMapper {
-
-    /**
-     * @var string
-     */
-    protected $entityClassName;
 
     /**
      * @var ClassInflector
      */
-    protected $entityInflector;
+    protected $entityCache;
 
-    public function __construct($entityClassName, ClassInflector $entityInflector)
+    /**
+     * This will need an instance of Container
+     * for its Container::make abilities
+     *
+     * @param EntityCache $entityCache
+     */
+    public function __construct(EntityCache $entityCache)
     {
-        $this->entityClassName = $entityClassName;
-        $this->entityInflector = $entityInflector;
+        $this->entityCache = $entityCache;
+    }
+
+    /**
+     * @param $entityClassName
+     * @param $data - Array? Object? Iterable? Illuminate\Database\ResultSet?
+     * @return object $entityClassName
+     */
+    public function hydrate($entityClassName, $data)
+    {
+        $inflectedEntity = $this->getEntity($entityClassName);
+        $inflectedEntity->
+
+        return $this->doHydrate($entityClassName, $inflectedEntity, $data);
+    }
+
+    /**
+     * @param $entityClassName - Our Domain Entity
+     * @param $inflectedEntity -
+     * @param $data
+     * @return object
+     */
+    protected function fillClass($entityClassName, $inflectedEntity, $data)
+    {
+        $reflectionClass = new ReflectionClass($entityClassName);
+
+        // Add data to reflected Class
+
+        return $reflectionClass->newInstanceWithoutConstructor();
+    }
+
+    protected function getEntity($entityClassName)
+    {
+        return $this->entityCache->get($entityClassName);
     }
 } 
