03ba221fddcd7480fc57882d80bc19e2f4255dba
fideloper <fideloper@gmail.com>
2014-04-15 08:26:52 -0400

    first signs of life in repository

--- src/EntityMapper/Repository.php
+++ src/EntityMapper/Repository.php
@@ -1,17 +1,116 @@
-<?php  namespace EntityMapper; 
+<?php  namespace EntityMapper;
+
+use Illuminate\Container\Container;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+use Illuminate\Database\ConnectionResolverInterface as Resolver;
 
 class Repository {
 
-    protected $table;
+    protected static $app;
 
-    public function find($id)
+    /**
+     * @var mixed
+     */
+    protected $entity;
+
+    /**
+     * @var EntityMapper
+     */
+    protected $mapper;
+
+    /**
+     * Database connection
+     * @var string
+     */
+    protected $connection;
+
+    /**
+     * The connection resolver instance.
+     *
+     * @var \Illuminate\Database\ConnectionResolverInterface
+     */
+    protected static $resolver;
+
+    public function __construct($entityClassName, EntityMapper $mapper)
     {
-        // Query
-        // And hydrate from result
+        $this->entity = $entityClassName;
+        $this->mapper = $mapper;
+    }
+
+    public function all() {}
+    public function find($id, $columns = array('*')){}
+    public function findMany($id, $columns = array('*')){}
+    public function findOrFail($id, $columns = array('*')) {}
+
+    public function save($entity) {}
+    public function delete($entity) {}
+    public function touch($entity) {}
+
+    public function query() {} // Builder?
+
+    // Date Logic?
+
+    public function on($connection = null)
+    {
+
+    }
+
+    public function getConnection()
+    {
+        return static::resolveConnection($this->connection);
+    }
+
+    public function getConnectionName()
+    {
+        return $this->connection;
+    }
+
+    public function setConnection($name)
+    {
+        $this->connection = $name;
+    }
+
+    public static function setConnectionResolver(Resolver $resolver)
+    {
+        static::$resolver = $resolver;
+    }
+
+    public static function getConnectionResolver()
+    {
+        return static::$resolver;
+    }
+
+    public static function resolveConnection($connection = null)
+    {
+        return static::$resolver->connection($connection);
     }
 
     public static function __callStatic($name, $arguments)
     {
         //if getXRepository, parse "X" and return repository (static) for that model class
+    }
+
+    public static function setApp(Container $app)
+    {
+        static::$app = $app;
+    }
+
+    public static function getApp()
+    {
+        return static::$app;
+    }
+
+    public static function getRepository($entityClassName)
+    {
+        $app = static::getApp();
+        $entityMapper = $app->make('\EntityMapper\EntityMapper');
+        $table = $app->make('\EntityMapper\Cache\EntityCache')->get($entityClassName);
+
+        if( $table->repository !== 'base' )
+        {
+            return $app->make($table->repository, [$entityClassName, $entityMapper]);
+        }
+
+        return new static($entityClassName, $entityMapper);
     }
 } 
