74af53fce11049623fb50fc9f749cdbc5eeb9b10
fideloper <fideloper@gmail.com>
2014-04-15 20:44:09 -0400

    added tests, sqlite memory

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,21 +1,3 @@
 <?php
 
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-class TestCase extends PHPUnit_Framework_TestCase {
-
-    protected $capsule;
-
-    public function setUp()
-    {
-        $this->capsule = new Capsule;
-
-        $this->capsule->addConnection([
-            'sqlite' => array(
-                'driver'   => 'sqlite',
-                'database' => ':memory:',
-                'prefix'   => '',
-            ),
-        ]);
-    }
-}
+class TestCase extends PHPUnit_Framework_TestCase {}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,43 @@ require_once('stubs/Email.php');
 require_once('stubs/Votes.php');
 require_once('stubs/User.php');
 require_once('stubs/UserRepository.php');
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'   => 'sqlite',
+    'database' => ':memory:',
+    'prefix'   => '',
+]);
+
+// Not actually required, despite
+// what the docs say
+$capsule->setAsGlobal();
+
+$capsule->schema()->create('users', function($table)
+{
+    $table->increments('id');
+    $table->string('name');
+    $table->string('email');
+    $table->integer('votes');
+});
+
+$capsule->table('users')->insert([
+    'name' => 'Chris',
+    'email' => 'chris@example.com',
+    'votes' => 20,
+]);
+
+$capsule->table('users')->insert([
+    'name' => 'Bob',
+    'email' => 'bob@example.com',
+    'votes' => 10,
+]);
+
+$capsule->table('users')->insert([
+    'name' => 'Dan',
+    'email' => 'dan@example.com',
+    'votes' => 0,
+]);
